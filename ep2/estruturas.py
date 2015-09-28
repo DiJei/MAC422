3c375685f8b6b8d22d0a2dff06804610780187ac
Fabio Brzostek Muller <tutofabio@uol.com.br>
2015-09-28 16:43:25 -0300

    Mudanças na estrutura da lista ligada, novas estruturas pra representar processos e acessos à memória, script com testes básicos pra ver se as estruturas funcionam, arquivo com esquema do que tem que fazer (sem considerar paginação)

---
+++ ep2/estruturas.py
@@ -0,0 +1,131 @@
+class Item:
+    """
+    Representa um item de uma lista ligada para gerenciar memória livre.
+    Guarda informações dizendo se o pedaço de memória representado pelo item
+    está livre ou sendo usado por um processo, qual o processo que está usando
+    ele (se algum estiver), e onde começa e qual o tamanho do pedaço de memória.
+    Além disso, guarda uma referência para o próximo item da lista (ou None se
+    esse item é o último).
+    """
+
+    prox = None
+    ant = None
+
+    def __init__(self, livre, proc_nome, inicio_mem, tamanho_mem):
+        self.livre = livre
+        self.proc_nome = proc_nome
+        self.inicio_mem = inicio_mem
+        self.tamanho_mem = tamanho_mem
+
+    def __str__(self):
+        descricao = ""
+        nome = self.proc_nome
+        len_nome_proc = len(self.proc_nome) + 2
+        len_inicio_mem = (len(str(self.inicio_mem)) + 2)
+        len_final_mem = (len(str(self.inicio_mem + self.tamanho_mem - 1)) + 2)
+
+        if self.livre:
+            len_nome_proc = 7
+            nome = "Livre"
+
+        descricao += " " + len_nome_proc * "_"
+        descricao += " " + len_inicio_mem * "_"
+        descricao += " " + len_final_mem * "_" + " _\n"
+        descricao += "|" + len_nome_proc * " "
+        descricao += "|" + len_inicio_mem * " "
+        descricao += "|" + len_final_mem * " " + "| |\n"
+        descricao += "| " + nome + " "
+        descricao += "| " + str(self.inicio_mem) + " "
+        descricao += "| " + str(self.inicio_mem + self.tamanho_mem - 1)
+        descricao += " |--->\n"
+        descricao += "|" + len_nome_proc * "_"
+        descricao += "|" + len_inicio_mem * "_"
+        descricao += "|" + len_final_mem * "_" + "|_|\n"
+
+        return descricao
+
+
+class Lista:
+    """
+    Representa uma lista ligada para gerenciar memória livre. Cada item da lista
+    é uma instância da classe Item. As instâncias da lista contêm uma referência
+    para o primeiro item da lista.
+    """
+
+    inicio = None
+
+    def __init__(self, inicio):
+        self.inicio = inicio
+        inicio.prox = None
+        inicio.ant = None
+
+    def __str__(self):
+        descricao = ""
+        if self.inicio:
+            descricao += str(self.inicio)
+        item = self.inicio.prox
+        while item:
+            descricao += str(item)
+            item = item.prox
+        descricao += "\nFim da lista\n"
+        return descricao
+
+    def adiciona_depois_de(self, item, novo_item):
+        novo_item.prox = item.prox
+        novo_item.ant = item
+        item.prox = novo_item
+
+    def remove_depois_de(self, item):
+        if item.prox:
+            if item.prox.prox:
+                item.prox.prox.ant = item
+            item.prox = item.prox.prox
+
+
+class Processo:
+    """
+    Representa um processo que vai rodar no sistema. Inclui o nome do processo,
+    o instante de tempo em que chegou (t0), o instante de tempo em que termina
+    (tf), quanto usa de memória em bytes (b), uma flag representando se o
+    processo está rodando ou não, outra flag representando se ele já terminou
+    ou não e uma lista de instâncias da classe Acesso contendo informações
+    sobre acesso à memória (onde, quando, já acessou)
+    """
+
+    rodando = False
+    terminou = False
+
+    def __init__(self, nome, t0, tf, b, acessos):
+        self.nome = nome
+        self.t0 = t0
+        self.tf = tf
+        self.b = b
+        self.acessos = acessos
+
+    def __str__(self):
+        descricao = self.nome + " chega no instante " + str(self.t0) + "s e "
+        descricao += "acaba no instante " + str(self.tf) + ", ocupa "
+        descricao += str(self.b) + " bytes; acessos à memória:\n"
+
+        for acesso in self.acessos:
+            descricao += str(acesso) + "\n"
+        return descricao
+
+
+class Acesso:
+    """
+    Representa um acesso a uma posição de memória, contendo informações sobre
+    qual a posição de memória acessada, o instante de tempo em que esse acesso
+    ocorre e se ele já ocorreu ou ainda não
+    """
+
+    ocorreu = False
+
+    def __init__(self, posicao, instante):
+        self.posicao = posicao
+        self.instante = instante
+
+    def __str__(self):
+        descricao = "Acesso à memória na posição " + str(self.posicao)
+        descricao += " aos " + str(self.instante) + "s"
+        return descricao
---
+++ ep2/testa.py
@@ -0,0 +1,31 @@
+from estruturas import *
+
+i1 = Item(True, '', 0, 100)
+i2 = Item(False, 'p1', 100, 100)
+i3 = Item(False, 'p2', 200, 100)
+i4 = Item(True, '', 300, 100)
+i5 = Item(False, 'p3', 400, 100)
+
+a1 = Acesso(0, 7)
+a2 = Acesso(7, 10)
+a3 = Acesso(10, 15)
+a4 = Acesso(100, 20)
+a5 = Acesso(200, 30)
+a6 = Acesso(202, 60)
+a7 = Acesso(333, 120)
+
+acessos1 = [a1, a2, a3, a4]
+acessos2 = [a5, a6, a7]
+
+proc1 = Processo("Proc1", 0, 100, 1024, acessos1)
+proc2 = Processo("Proc2", 120, 500, 16384, acessos2)
+
+lista = Lista(i1)
+lista.adiciona_depois_de(lista.inicio, i2)
+lista.adiciona_depois_de(lista.inicio.prox, i3)
+lista.adiciona_depois_de(lista.inicio.prox.prox, i4)
+lista.adiciona_depois_de(lista.inicio.prox.prox.prox, i5)
+
+print("lista:", lista)
+print("processo 1:", proc1)
+print("processo 2:", proc2)
